6fa1285fb4150ca86ef19e3e5d1c05f9fe68fd11
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-12-27 14:36:45 -0300

    proyect start

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'standard-with-typescript',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json'],
+	},
+	plugins: ['react'],
+	rules: {},
+};
