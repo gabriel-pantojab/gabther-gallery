321dc4ec90896c96d5cb0d0d1280eb54ad27711a
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2024-02-10 22:46:24 -0300

    [DELETE] supabase context

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import { RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import { router } from './routes/router';
-import SupabaseProvider from './context/supabaseContext';
 import UserProvider from './context/userContext';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<SupabaseProvider>
-			<UserProvider>
-				<RouterProvider router={router} />
-			</UserProvider>
-		</SupabaseProvider>
+		<UserProvider>
+			<RouterProvider router={router} />
+		</UserProvider>
 	</React.StrictMode>,
 );
